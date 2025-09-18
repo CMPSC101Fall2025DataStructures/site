88337975a4cb264f3087a130c338c0f2ad93f635
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2025-09-18 11:46:15 -0400

    add lab 03 url

--- materials/commands.sh
+++ materials/commands.sh
@@ -1,3 +1,3 @@
-quarto render 2_introToPython.qmd --to pdf
-quarto render 3_introToPython.qmd --to pdf
+quarto render 3_i_introToPython_solutions.qmd  --to pdf
+quarto render 3_ii_introToPython_solutions.qmd --to pdf
 
